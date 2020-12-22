35f524faf676928f46530f6e5b2cb61f125658c0
otofu2713@gmail.com <ec2-user@ip-172-31-45-18.ec2.internal>
2020-12-22 03:51:39 +0000

    [Finish]4-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6e4566f29a22b595efe2b5d3d0c93057f131c077aac0352b63cd3f833edf305239ecf06f8c7b012b73036554ab7673545a9e2947d464a94a8d01c00f851c3a2b'
